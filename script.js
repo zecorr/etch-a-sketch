5583fbc6e34a4f7dd4c73ce802b864f22747506d
zecorr <zecorr88@gmail.com>
2023-11-10 17:38:51 -0300

    Create 16 x 16 grid

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hey bro")
+const body = document.querySelector("body")
+
+const createLine = function(){
+    const line = document.createElement("div")
+    body.appendChild(line)
+    line.style.cssText = "padding: 0; display: flex; justify-content: center;"
+    const createSquare = function (){
+        const square = document.createElement("div")
+        line.appendChild(square)
+        square.style.cssText = "border: 1px solid red; padding: 14px"
+    }
+    for(let i = 0; i < 16; i++){
+        createSquare()
+    }
+}
+
+for(let i = 0; i < 16; i++){
+    createLine()
+}
+
