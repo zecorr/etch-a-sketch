d8473bf93eb039591cb99cd51a8635823afb9a29
zecorr <zecorr88@gmail.com>
2023-11-10 18:37:06 -0300

    Move CSS to style.css

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 const body = document.querySelector("body")
 
+// Create a line div with 16 square divs inside
 const createLine = function(){
     const line = document.createElement("div")
     body.appendChild(line)
-    line.style.cssText = "padding: 0; display: flex; justify-content: center;"
+    line.setAttribute("class", "line")
+
     const createSquare = function (){
         const square = document.createElement("div")
         line.appendChild(square)
-        square.style.cssText = "border: 1px solid red; padding: 14px"
+        square.setAttribute("class", "square")
+        square.addEventListener("mouseover", () => {square.style.backgroundColor = "darkblue"})
     }
     for(let i = 0; i < 16; i++){
         createSquare()
     }
 }
 
+// Create 16 lines with squares
 for(let i = 0; i < 16; i++){
     createLine()
 }
-
